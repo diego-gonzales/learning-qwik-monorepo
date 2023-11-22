e5ffd2d0fb7079a76d6f4ecd2930d114325a46b7
Diego Gonzales <diegoegj14@gmail.com>
2023-11-22 16:44:47 -0500

    Update global.css and login page

--- projects/08-clon-uppbeat/src/routes/auth/login/index.tsx
+++ projects/08-clon-uppbeat/src/routes/auth/login/index.tsx
@@ -1,5 +1,15 @@
-import { component$ } from '@builder.io/qwik';
+import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
-  return <div>Login page</div>;
+  return (
+    <div class="grid min-h-screen place-items-center px-5">
+      <form class="flex w-full max-w-xs flex-col gap-3">
+        <input class="qwik-input-src" type="text" placeholder="Email" />
+        <input class="qwik-input-src" type="text" placeholder="Password" />
+        <button class="qwik-button-primary self-center" type="submit">
+          Iniciar sesión
+        </button>
+      </form>
+    </div>
+  );
 });
