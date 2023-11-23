a0f803913d2095ba75859d42c58b2a620b329eee
Diego Gonzales <diegoegj14@gmail.com>
2023-11-23 12:08:52 -0500

    Fix import and add onGet handler for authentication layout

--- projects/08-clon-uppbeat/src/routes/auth/layout.tsx
+++ projects/08-clon-uppbeat/src/routes/auth/layout.tsx
@@ -1,4 +1,12 @@
-import { Slot, component$ } from '@builder.io/qwik';
+import { Slot, component$ } from "@builder.io/qwik";
+import { type RequestHandler } from "@builder.io/qwik-city";
+import { DATA_KEY } from "~/constants";
+
+export const onGet: RequestHandler = async ({ cookie, redirect }) => {
+  const dataFromCookie = cookie.get(DATA_KEY)?.value;
+
+  if (dataFromCookie) throw redirect(308, "/");
+};
 
 export default component$(() => {
   return <Slot />;
